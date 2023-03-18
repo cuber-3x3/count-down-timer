84100c3b1c22a37165ec2038db108ce8e775ebd4
cuber-3x3 <127592093+cuber-3x3@users.noreply.github.com>
2023-03-18 15:08:51 +0800

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,15 +1,39 @@
-let 跳绳时间 = 0
-input.onButtonPressed(Button.A, function () {
-    跳绳时间 = 60000
-    while (跳绳时间 > 0) {
-        basic.pause(1000)
-        跳绳时间 += -1000
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "START") {
+        跳绳个数 = 0
+        basic.showLeds(`
+            # # # # #
+            . # # # .
+            . . # . .
+            . # . # .
+            # # # # #
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 300, 1, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+        跳绳时间 = 60000
+        while (跳绳时间 > 0) {
+            basic.pause(1000)
+            跳绳时间 += -1000
+        }
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
+        radio.sendString("FINISH")
+        跳绳时间 = 0
+        basic.showLeds(`
+            # # # # #
+            . # . # .
+            . . # . .
+            . # # # .
+            # # # # #
+            `)
     }
-    basic.showLeds(`
-        # # # # #
-        . # # # .
-        . . # . .
-        . # # # .
-        # # # # #
-        `)
 })
+input.onGesture(Gesture.ThreeG, function () {
+    if (跳绳时间 > 0) {
+        跳绳个数 += 1
+        radio.sendNumber(跳绳个数)
+    }
+})
+let 跳绳时间 = 0
+let 跳绳个数 = 0
+radio.setGroup(31)
+music.setVolume(249)
+跳绳个数 = 0
